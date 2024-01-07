71dbcb3c2a846e0ff8b4a3f0e8c237d6d7f1d9ec
MaheshKumbhar <maheshkumbhar@Maheshs-MacBook-Air.local>
2024-01-07 17:11:23 +0530

    BQ changes

--- gcp/Bigquery_using_schema_json_file/resources.tf
+++ gcp/Bigquery_using_schema_json_file/resources.tf
@@ -1,11 +1,13 @@
 resource "google_bigquery_dataset" "dataset1" {
-  dataset_id = "dataset-2-fro-terraform"
+    dataset_id = "dataset-2-fro-terraform"
 }
 
+data "template_file" "schema" {
+    template = file("schema.json")
+}
 
 resource "google_bigquery_table" "example_table" {
-  dataset_id = google_bigquery_dataset.dataset1.dataset_id
-  table_id   = "table-using-schema-json-file"
-
-  schema = jsonencode("Terraform/terraform/gcp/Bigquery_using_schema_json_file/schema.json")
+    dataset_id = google_bigquery_dataset.dataset1.dataset_id
+    table_id   = "table-using-schema-json-file"
+    schema = jsonencode(data.template_file.schema.rendered)
 }
