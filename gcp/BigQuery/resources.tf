446d9a9a6f3ff40f50a593e4c61a22d8e9ea6e1c
MaheshKumbhar <maheshkumbhar@Maheshs-MacBook-Air.local>
2024-01-07 16:45:57 +0530

    created BQ table using schema

--- gcp/BigQuery/resources.tf
+++ gcp/BigQuery/resources.tf
@@ -9,4 +9,22 @@ resource "google_bigquery_dataset" "dataset" {
 resource "google_bigquery_table" "table" {
     dataset_id = google_bigquery_dataset.dataset.dataset_id
     table_id = "table_from_terraform"
+    
+      schema = <<EOF
+[
+  {
+    "name": "permalink",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "The Permalink"
+  },
+  {
+    "name": "state",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "State where the head office is located"
+  }
+]
+EOF
+
 }
